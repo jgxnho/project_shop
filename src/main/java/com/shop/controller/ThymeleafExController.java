e4243050af095fa44d175b64acedf9e10451f053
gxnho <gxnho@naver.com>
2024-04-22 20:51:18 +0900

    4.22 JoinTest

--- src/main/java/com/shop/controller/ThymeleafExController.java
+++ src/main/java/com/shop/controller/ThymeleafExController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping(value = "/thymeleaf")
@@ -50,7 +51,8 @@ public class ThymeleafExController {
 
     @GetMapping(value = "/ex04")
     public String thymeleafExample04(Model model) {
-        ArrayList<Object> itemDtoList = new ArrayList<>();
+
+        List<ItemDto> itemDtoList = new ArrayList<>();
 
         for (int i = 1; i <= 10; i++) {
             ItemDto itemDto = new ItemDto();
@@ -61,12 +63,13 @@ public class ThymeleafExController {
 
             itemDtoList.add(itemDto);
         }
+
         model.addAttribute("itemDtoList", itemDtoList);
         return "thymeleafEx/thymeleafEx04";
     }
 
     @GetMapping(value = "/ex05")
-    public String thymeleafExample05(Model model) {
+    public String thymeleafExample05() {
         return "thymeleafEx/thymeleafEx05";
     }
 
@@ -75,5 +78,10 @@ public class ThymeleafExController {
         model.addAttribute("param1", param1);
         model.addAttribute("param2", param2);
         return "thymeleafEx/thymeleafEx06";
+    }
+
+    @GetMapping(value = "/ex07")
+    public String thymeleafExample07() {
+        return "thymeleafEx/thymeleafEx07";
     }
 }
